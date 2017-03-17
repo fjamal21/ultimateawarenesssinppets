99bc8fc63aaac99ebf1a0da949ae9ffccf026384
azx0987 <fjamal21@gmail.com>
2017-03-16 20:22:49 -0400

    ultimateawarenesssnippets

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The Ultimate Awareness Giveaway</title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="script.js"></script>
+
+	<meta property="og:url"                content="http://www.ultimateawarenessgiveaway.com" />
+	<meta property="og:type"               content="contest" />
+	<meta property="og:title"              content="The Ultimate Awareness Giveaway" />
+	<meta property="og:description"        content="Join the cause for your chance to win a trip to Cabo" />
+	<meta property="og:image"              content="" />
+</head>
+<body>
+<div id="body-wrap">
+	<div class="page-wrapper">
+		<div id="section-wrap">
+			<h1 class="hidden">Ultimate Awareness Giveaway Contest</h1>
+		
+		<div id="container-left">
+			<img id="logo" src="images/logo-giveaway.png" alt="Ultimate Giveaway Contest Logo">
+			<p id="left-text">Join The Movement For Your Chance <br>To Win A Trip For 2 To<br><span id="cabo-san-lucas">Cabo San Lucas</span><br><span id="green-text">me</span><span id="white-text">xi</span><span id="red-text">co</span></p>
+		</div><!--close container-left-->
+		<div id="container-right">
+			<!--
+			<button type="button" class="button" name="joinButton">Join The Movement</button>
+			-->
+			<div id="div-countdown">
+				<h2>The Next Contest Starts in</h2>
+				<div id="clock"></div>
+					<div id="units">
+						<span>Days</span>
+						<span>Hours</span>
+						<span>Minutes</span>
+						<span>Seconds</span>
+					</div>
+			</div>
+			<p class="right-text" id="come-back">Come Back on April 18<span id="superscript">th</span> 2017 to Enter For Your Chance to Win Our Grand Prize.</p>
+			<p class="right-text" id="supporters">Our Supporters</p>
+			<p class="right-text" id="how-to" >How to Enter:<br>
+				Enter by clicking the link below and filling in the required information. The grand prize will be awarded to the winner of a drawing which will take place on INSERT DATE. Giveaway closes at 12:00 PM on July 15th. Entrance is 100% free so you have absolutely nothing to lose.</p>
+			</div><!--close contiainer-right-->
+		</div><!--close section-wrap-->
+		<a class="button" href="#popup1">Contest Rules & Regulations</a>
+
+
+<div id="popup1" class="overlay">
+    <div class="popup">
+        <h2>The Ultimate Awareness Giveaway Terms & Conditions</h2>
+        <a class="close" href="#">&times;</a>
+        <div class="rules">
+            <p>JUNE 29, 2016 through JULY 15, 2016<br>
+			<br>
+			NO PURCHASE IS NECESSARY TO ENTER OR WIN THIS GIVEAWAY. VOID WHERE PROHIBITED BY LAW.<br>
+			<br>
+			TERMS:<br>
+			The entry period for THE ULTIMATE AWRNESS GIVEAWAY.
+			Commences 12:00am EST on JUNE 29, 2016 and ends on JULY 15, 2016 at  12:00pm EST.<br>
+			<br>
+			The Giveaway is sponsored by Golf for Good Inc. 44 Laird Drive Toronto (sometimes hereinafter referred to as the "Sponsors").<br>
+			<br>
+			ELIGIBILITY:<br>
+			This Giveaway is open to legal residents of Canada and the United States. Entrants must be 18 years or older as of date of entry. Employees of the Sponsor; employees of any promotional companies or advertising agencies participating in the Giveaway and their respective subsidiaries and affiliates; any persons involved in the creation, development, or implementation of this Giveaway; and any other persons or entities directly associated with this Giveaway and any members of their immediate families or households are ineligible to participate. <br>
+			<br>
+			Limit one entry per email address.<br>
+			GRAND PRIZE/VALUE: One (1) Grand Prize will be awarded. The prize includes a seven (7) night stay in a one-bedroom suite, sleeping up to six (6) people, at Casa Dorado Los Cabos Resort and Spa. The Prize can be used anytime from September 2016 to September 2017. Approximately package retail value of then thousand dollars ($10,000).  The schedule is as follows:
+			No part of the Grand Prize is transferable, assignable, or redeemable for cash or any other consideration, and no substitutions will be allowed except by Sponsor. Sponsor reserves the right to substitute prizes of equal or greater value or nature if advertised prize is not available. Grand Prize winner is solely responsible for the payment of all applicable taxes and additional fees. <br>
+			<br>
+			HOW TO ENTER:<br>
+			Enter the giveaway by visiting ultimateawarnessgiveaway.com. No purchase necessary.<br>
+			Your full name, street address, city, state, zip code, email address, and telephone number are required for entry. Each entrant is responsible for the accuracy of the information included in his/her entry. All entries will become the property of Sponsor. Incomplete or illegible entries will be disqualified.<br>
+			<br>
+			RANDOM DRAWING/ODDS OF WINNING/WINNER:<br>
+			The potential winner of the Giveaway will be selected by Sponsor in a random drawing on Wednesday July 15, 2016 at 12pm EST from all eligible entries received. Entrants need not be present to win. The potential Grand Prize winner will be notified via email and/or telephone on Wednesday July 15, 2016 using the contact information provided on the winner's entry form. The odds of winning the Grand Prize will be determined by the actual number of eligible entries received. Before being declared a winner, selected entrant must first correctly answer without assistance of any kind, whether mechanical or otherwise, a time-limited mathematical skill-testing question to be administered at a mutually convenient time by telephone. <br>
+			<br>
+			Potential Grand Prize winner will be required to sign and return an Affidavit of Eligibility, Liability Release, and Publicity Release within 48 hours of notification attempt. If Sponsor is unable to reach the potential Grand Prize winner or the potential Grand Prize winner does not return the signed forms within the 48 hour period, the potential winner will be disqualified and will forfeit any claim to the Grand Prize. Sponsor may, in its sole discretion, award the Grand Prize to an alternate winner selected at random from the remaining eligible entries received.<br>
+			<br>
+			The name and province or state of residence of the Grand Prize winner will be available after Wednesday July 15, 2016. The sponsor have the opportunity to message on social media platforms Sponsor’s selection of the winner and award of the Grand Prize shall be final and conclusive.<br>
+			<br>
+			CAUTION:<br>
+			Any attempt by any person to undermine legitimate operation of this giveaway may constitute a violation of criminal and civil laws. Sponsor reserves the right to seek damages from and/or to prosecute any person who makes any such attempt to the fullest extent permitted by law. Any such tampering also will result in ineligibility to win the Grand Prize in the Giveaway.
+			<br>
+			APPLICABLE LAW:<br>
+			This Giveaway is void wherever prohibited or restricted by law and is subject to all federal, state, and local laws and regulations. By submitting an entry form, entrants agree to abide by all applicable federal, state, and local laws and regulations.<br>
+			<br>
+			CONDITIONS:<br>
+			By participating in this giveaway, entrants agree to be bound by these Official Rules and the decisions of Sponsors, which are final in all matters relating to this Giveaway.<br>
+			Sponsors are in no way liable for any damage, loss, or injury resulting from computer malfunctions, misdirected or incomplete entries, or acceptance and use of the Grand Prize. The winner assumes liability for injuries caused or claimed to be caused by participation in the Giveaway or the use or misuse of the Grand Prize.  Sponsors reserve the right, in its sole discretion, to cancel, modify or suspend the Giveaway should virus, bugs, or other causes beyond the control of Sponsors corrupt the administration, security, fairness, integrity or proper play of the Giveaway. Sponsors may elect, in the sole exercise of its discretion, to award the Grand Prize from all eligible entries received prior to cancellation or suspension.<br>
+			<br>
+			By entering this Giveaway, each entrant hereby voluntarily and irrevocably releases, holds harmless and agrees not to institute any claim against Sponsors, or any other persons involved in the creation, development, or implementation of this Giveaway, and/or any of such person’s respective affiliates, officers, directors, shareholders, employees and agents, from any losses, damages, or expenses arising out of or relating to any and all injuries, losses or damages of any kind (including without limitation, personal injury, death and/or property loss) arising from or in connection with, either directly or indirectly, (1) the awarding, acceptance, receipt, possession, use and/or misuse of the prize awarded herein; and/or (2) participation in the giveaway or any prize related activities. Entrants waive any and all claims that any aspect of the Official Rules or the Giveaway is ambiguous, inequitable, or in error.<br>
+			Under no circumstances will any entrant be permitted to obtain awards for, and each entrant hereby waives all rights to claim, punitive, incidental or consequential damages, or any other damages, including attorneys’ fees, and entrant further waives all rights to have damages multiplied or increased.
+			By accepting the Grand Prize, the winner grants Sponsors permission to use his or her name and likeness for advertising and promotional purposes without any additional compensation (except where prohibited by law).<br>
+			<br>
+			SPONSOR:<br><br>
+			Golf for Good Inc. 44 Laird Drive, Toronto, ON, M4G 3T2<br>
+			</p>
+        </div>
+    </div>
+</div>
+	</div>
+
+
+
+
+	<footer class="page-wrapper" id="footer">
+		<p id="disp-none" class="footer-text" id="col2"><a href="#popup1">Contest Rules & Regulations</a></p>
+		<p class="footer-text" id="col1">Follow us on Social Media</p>
+		
+		
+
+		<div id="social-block">
+			<a href="https://www.facebook.com/Ultimate-Awareness-Giveaway-812689868863507/"><div class="social" id="fb"><span id="fb-link"></span></div></a>
+			<!--<a href="instagram.com"><div class="social" id="insta"><span id="insta-link"></span></div></a>-->
+			<a href="https://twitter.com/ULTAwarness"><div class="social" id="twitter"><span id="twitter-link"></span></div></a>
+		</div>
+
+		 
+	</footer>
+		<div id="copyright-bar">
+			<div class="page-wrapper">
+				<p>© MyG4G.org, 2016.</p>
+			</div>
+		</div>
+</div>	
+</body>
+</html>
